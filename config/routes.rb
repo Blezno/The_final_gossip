663c93807105ea485e672057dd28bb3a3845ed5f
blezno <gonzaleznovoa.b@protonmail.com>
2022-11-03 17:01:10 +0100

    au cas ou

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,9 @@ Rails.application.routes.draw do
   get 'gossip/new'
   get 'gossip/show'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   get '/', to:'main#home'
   get '/welcome/:first_name', to: 'main#welcome'
 
   resources :gossips
-  
+
 end
